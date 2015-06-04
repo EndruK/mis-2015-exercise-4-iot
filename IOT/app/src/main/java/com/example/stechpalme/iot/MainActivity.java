44b7bb60164cfa9331849fef3b01140dc8f17e05
André Karge <andre.karge@gmail.com>
2015-06-04 18:40:10 +0200

    added plot for several content stuff

--- IOT/app/src/main/java/com/example/stechpalme/iot/MainActivity.java
+++ IOT/app/src/main/java/com/example/stechpalme/iot/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.stechpalme.iot;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
     private NfcAdapter myNFCAdapter;
+    private TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myTextView = (TextView) findViewById(R.id.myTextView1);
         myNFCAdapter = NfcAdapter.getDefaultAdapter(this);
         if(myNFCAdapter == null) {
             Toast t = Toast.makeText(this, "NFC is not supported by this device", Toast.LENGTH_LONG);
@@ -26,5 +32,25 @@ public class MainActivity extends Activity {
             Toast t = Toast.makeText(this,"NFC is disabled! Please enable :)",Toast.LENGTH_LONG);
             t.show();
         }
+        readNFC(getIntent());
+    }
+    private void readNFC(Intent intent) {
+        String output = "";
+        output += "Type: " + intent.getType();
+        output += "\nAction: " + intent.getAction();
+        output += "\nData: " + intent.getData();
+        output += "\nDataString: " + intent.getDataString();
+        Parcelable[] rawMsg = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if(rawMsg != null) {
+            output += "\n\nExtraMsg\n";
+            NdefMessage[] msgs = new NdefMessage[rawMsg.length];
+            for(int i = 0; i<rawMsg.length; ++i) {
+                msgs[i] = (NdefMessage) rawMsg[i];
+            }
+            for(int i = 0; i<msgs.length; ++i) {
+                output += "message " + i + ": " + msgs[i].toString() + "\n";
+            }
+        }
+        myTextView.setText(output);
     }
 }

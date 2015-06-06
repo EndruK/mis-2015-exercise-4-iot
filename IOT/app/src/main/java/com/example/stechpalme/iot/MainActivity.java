8cf6bd0c8968a050ca5c0e159c1c2081d88c1cde
André Karge <andre.karge@gmail.com>
2015-06-06 17:53:07 +0200

    added button for raw data and support for mifare ultralight

--- IOT/app/src/main/java/com/example/stechpalme/iot/MainActivity.java
+++ IOT/app/src/main/java/com/example/stechpalme/iot/MainActivity.java
@@ -7,23 +7,19 @@ import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.app.Activity;
 import android.nfc.Tag;
-import android.nfc.tech.MifareClassic;
 import android.nfc.tech.MifareUltralight;
 import android.nfc.tech.Ndef;
 import android.nfc.tech.NfcA;
-import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.MalformedParameterizedTypeException;
+import java.nio.charset.Charset;
+
 
 /*
 TODO: 3rd part of ex.1
-TODO: TAG ID?
-TODO: technology?
-TODO: Data type/MIME Type of NDEF DATA
  */
 public class MainActivity extends Activity {
 
@@ -90,76 +86,69 @@ public class MainActivity extends Activity {
         String output = "";
         Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         if(tag != null) {
-            output += "Technology:\n";
+            output += "Tag ID: " + bytesToHexString(tag.getId());
+            output += "\n\nTechnology:\n";
             String[] list = tag.getTechList();
-            for(String el : list) {
+            for (String el : list) {
                 output += el + "\n";
             }
-            output += "\nTag ID: " + bytesToHexString(tag.getId());
-        }
-        Parcelable[] rawMsg = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        if(rawMsg != null) {
-            output += "\n\nNDEF Messages:";
-            NdefMessage[] msgs = new NdefMessage[rawMsg.length];
-            for(int i = 0; i<rawMsg.length; ++i) {
-                msgs[i] = (NdefMessage) rawMsg[i];
-            }
-            for(int i = 0; i<msgs.length; ++i) {
-                for(int j=0; j<msgs[i].getRecords().length; ++j) {
-                    byte[] payload = msgs[i].getRecords()[j].getPayload();
-                    String parsedPayload = "";
-                    String type =  msgs[i].getRecords()[j].toMimeType();
-                    if(type != null) {
-                        try {
-                            parsedPayload = parsePayload(payload);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+            Parcelable[] rawMsg = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsg != null) {
+                output += "\nNDEF Messages:";
+                NdefMessage[] msgs = new NdefMessage[rawMsg.length];
+                for (int i = 0; i < rawMsg.length; ++i) {
+                    msgs[i] = (NdefMessage) rawMsg[i];
+                }
+                for (int i = 0; i < msgs.length; ++i) {
+                    for (int j = 0; j < msgs[i].getRecords().length; ++j) {
+                        byte[] payload = msgs[i].getRecords()[j].getPayload();
+                        String parsedPayload = "";
+                        String type = msgs[i].getRecords()[j].toMimeType();
+                        if (type != null) {
+                            try {
+                                parsedPayload = parsePayload(payload);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            type = "no type";
+                            try {
+                                parsedPayload = msgs[i].getRecords()[j].toUri().toString();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
+                        output += "\nMessage #" + j + " (Mime:" + type + "):\n" + parsedPayload;
                     }
-                    else {
-                        type = "no type";
-                        try {
-                            parsedPayload = msgs[i].getRecords()[j].toUri().toString();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    output += "\nMessage #" + j + " (Mime:" + type + "):\n" + parsedPayload;
                 }
             }
-        }
-        /*
-        if(myNFCAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
-            Tag tmp = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            MifareClassic mfc = MifareClassic.get(tmp);
-            byte[] data = null;
-            try {
-                mfc.connect();
-                int sectors = mfc.getSectorCount();
-                for(int i=0; i<sectors; ++i) {
-                    if(mfc.authenticateSectorWithKeyA(i,MifareClassic.KEY_DEFAULT)) {
-                        int blocks = mfc.getBlockCount();
-                        int index = 0;
-                        for(int j=0; j<blocks; ++j) {
-                            index = mfc.sectorToBlock(i);
-                            data = mfc.readBlock(index);
-                            index++;
-                        }
-                    }
-                }
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            if(data != null) {
-                output += "\n\nbytes:\n" + data.toString();
-                output += "\n\nhex:\n" + bytesToHexString(data);
-            }
-            else {
-                Toast t = Toast.makeText(this,"hmm leider nein",Toast.LENGTH_LONG);
-                t.show();
-            }
-        }*/
+            MifareUltralight mifare = MifareUltralight.get(tag);
+            if (mifare != null) {
+                try {
+                    mifare.connect();
+                    byte[] payload = mifare.readPages(4);
+                    System.out.println(parsePayload(payload));
+                    //String tmp = new String(payload, Charset.forName("US-ASCII"));
+                    //System.out.println(tmp);
 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast t = Toast.makeText(this, "could not connect to mifare ultralight", Toast.LENGTH_LONG);
+                    t.show();
+                } finally {
+                    try {
+                        mifare.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast t = Toast.makeText(this,"could not cloase mifare ultralight",Toast.LENGTH_LONG);
+                        t.show();
+                    }
+                }
+            }
+        }
+        else {
+            output = "Please Scan a NFC Tag";
+        }
         myTextView.setText(output);
     }
     // from http://stackoverflow.com/questions/14607425/read-data-from-nfc-tag
@@ -182,7 +171,6 @@ public class MainActivity extends Activity {
         for (int i = 0; i < src.length; i++) {
             buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
             buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
-            //System.out.println(buffer);
             stringBuilder.append(buffer);
         }
 
